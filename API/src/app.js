5663bf4a484f481c85650acc4eb1fd3724176053
Lautaro Boiola <lboiola@gmail.com>
2022-05-09 12:14:35 -0300

    Firs route created

--- API/src/app.js
+++ API/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mainRoutes = require('./routes/mainRoutes')
 
-app.listen(3000, () =>{
+app.listen(process.env.PORT || 3000, () =>{
     console.log('Server up in port 3000');
 })
 
--- API/src/routes/mainRoutes.js
+++ API/src/routes/mainRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const Router = express.Router()
+const router = express.Router()
 const mainController = require('../controllers/mainController')
 
 // Main routes
-Router.get('/', mainController.index)
+router.get('/', mainController.index)
 
-Router.get('/api', mainController.api)
+router.get('/api', mainController.api)
 
-module.exports = Router
+module.exports = router
