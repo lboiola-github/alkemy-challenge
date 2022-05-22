210c0c92b82548e08514f3a2d1186d275b18a834
Lautaro Boiola <lboiola@gmail.com>
2022-05-22 20:02:31 -0300

    Create of temporary endpoints with fixed data

--- API/src/routes/mainRoutes.js
+++ API/src/routes/mainRoutes.js
@@ -7,4 +7,10 @@ router.get('/', mainController.index)
 
 router.get('/api', mainController.api)
 
+router.get('/api/create', mainController.create)
+
+router.get('/api/user/:id', mainController.user)
+
+router.get('/api/user/create', mainController.newUser)
+
 module.exports = router
