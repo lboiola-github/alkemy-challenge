b7f371aeccef3ade3bf9af044a8b3e34b286f2af
Lautaro Boiola <lboiola@gmail.com>
2022-05-25 21:17:40 -0300

    Register and login almost done. Remaining minor issues

--- API/src/controllers/userController.js
+++ API/src/controllers/userController.js
@@ -1,27 +1,41 @@
 const db = require('../database/models')
+const bcrypt = require('bcryptjs')
 
 const userController = {
     register: (req, res) => {
         db.User.create({
-            name: "nuevo al bardoooo",
-            email: "reciencreado@email.com",
-            password: "asd1asd1asd2asd3asd3"
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10)
         })
         .then(newUser => {
             res.send(newUser)
         })
     },
 
-    login: (req, res) => {
-        db.User.findOne({
+    login: async (req, res) =>  {
+        let password = req.body.password
+        let userFinded = await db.User.findOne({
             where: {
-                email: req.body.email,
-                password: req.body.password
+                email: req.body.email
             }
         })
             .then(user => {
-                res.json(user)
+                return user
             })
+
+        if (userFinded && bcrypt.compareSync(password, userFinded.password)){
+            req.session.user = userFinded
+            res.json({
+                meta: {
+                    status: 200,
+                    total: 1,
+                    url: '/user/login'
+                },
+                data: req.session.user})
+        } else {
+            res.json("Invalid credentials")
+        }
     },
 
     logout: (req, res) => {},
